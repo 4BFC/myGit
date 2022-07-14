e97c160a03c1e254f5369d3bd3b8fdd92e60ae05
4Bee <4bee.code@gmail.com>
2022-07-15 02:05:09 +0900

    2nd commit

---
+++ Assets/scripts/Ctrl/Ctrl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctrl : MonoBehaviour
+{
+    float h;
+    float v;
+    
+    int p = 10;    
+    
+    Rigidbody rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        Vector3 cur = new Vector3(h,v,0) * p;
+        rigid.velocity = cur;
+        
+        
+    }
+}
