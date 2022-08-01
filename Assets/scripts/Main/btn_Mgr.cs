9668a90d87c93aad819328a10013516a166d8aed
4Bee <4bee.code@gmail.com>
2022-08-02 00:21:56 +0900

    5th comomit
    5th commit

--- Assets/scripts/Main/btn_Mgr.cs
+++ Assets/scripts/Main/btn_Mgr.cs
@@ -8,13 +8,16 @@ using UnityEngine.UI;
 public class btn_Mgr : MonoBehaviour
 {
     public Button selectBtn, set_btn;
-    public GameObject[] skin_btn;
-
+    public Button[] skin_btn;
+    public bool[] check;
     // Start is called before the first frame update
     void Start()
     {
         selectBtn.onClick.AddListener(sceneChange);
-        
+        skin_btn[0].onClick.AddListener(skin_Btn0);
+        skin_btn[1].onClick.AddListener(skin_Btn1);
+        skin_btn[2].onClick.AddListener(skin_Btn2);
+
         //skin_Img = new GameObject[3];
     }
 
@@ -29,8 +32,34 @@ public class btn_Mgr : MonoBehaviour
         SceneManager.LoadScene("Game");
     }
     
-    void skin_Btn()
+    void skin_Btn0()//나중에 loadScene을 할 때 조건문으로 사용 할 것
     {
-
+        if (skin_btn[0])
+        {
+            check[0] = true;
+            check[1] = false;
+            check[2] = false;
+            Debug.Log(check[0]);
+        }
+    }
+    void skin_Btn1()
+    {
+        if (skin_btn[1])
+        {
+            check[0] = false;
+            check[1] = true; 
+            check[2] = false;
+            Debug.Log(check[1]);
+        }
+    }
+    void skin_Btn2()
+    {
+        if (skin_btn[2])
+        {
+            check[0] = false;
+            check[1] = false; 
+            check[2] = true;
+            Debug.Log(check[2]);
+        }
     }
 }
